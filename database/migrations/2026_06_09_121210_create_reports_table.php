5029ca442ed8ed7be1f3519131d3cc095c88cf2b
maylia-15 <rabiahriska8@gmail.com>
2026-06-09 21:59:23 +0800

    Memperbaiki Typo

--- database/migrations/2026_06_09_121210_create_reports_table.php
+++ database/migrations/2026_06_09_121210_create_reports_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('catagories')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
 
             $table->string('judul');
             $table->text('deskripsi');
